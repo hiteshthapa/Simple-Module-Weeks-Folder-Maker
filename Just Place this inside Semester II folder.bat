e7cdab11b7d417e18f2455871a0435a45a1f901e
styrka01 <88643565+styrka01@users.noreply.github.com>
2022-02-25 07:49:09 +0545

    Add files via upload

---
+++ Just Place this inside Semester II folder.bat
@@ -0,0 +1,13 @@
+@echo off
+cd
+md 5CS022-Distributed-and-Cloud-Systems-Programming 5CS020-Human-Computer-Interaction 5CS024-Collaborative-Development
+cd 5CS022-Distributed-and-Cloud-Systems-Programming
+md Week-1 Week-2 Week-3 Week-4 Week-5 Week-6 Week-7 Week-8 Week-9 Week-10 Week-11 Week-12
+cd..
+cd 5CS020-Human-Computer-Interaction
+md Week-1 Week-2 Week-3 Week-4 Week-5 Week-6 Week-7 Week-8 Week-9 Week-10 Week-11 Week-12
+cd..
+cd 5CS024-Collaborative-Development\
+md Week-1 Week-2 Week-3 Week-4 Week-5 Week-6 Week-7 Week-8 Week-9 Week-10 Week-11 Week-12
+
+
